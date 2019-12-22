9b0bac768c041a2d2761cb37b55947e09e7ad475
Christian Bewernitz <coder@karfau.de>
2019-12-22 09:02:11 +0100

    test: test for passing no argument
    - additional doc comments

--- examples/echo.js
+++ examples/echo.js
@@ -1,4 +1,15 @@
 module.exports = {
+  /**
+   * The "Hello, world" of a `run` method, just that is uses `JSON.stringify`
+   * on all passed arguments, so it is more easy to see how many arguments have been passed.
+   *
+   * THis way it is of course also more easy to parse the output with other shell tools like `jq`.
+   *
+   * Usage:
+   * `npx runex examples/echo` // []
+   * `npx runex examples/echo Hello world` // ["Hello", "world"]
+   * `npx runex examples/echo 'Hello, world'` // ["Hello, world"]
+   */
   run: function (...args) {
     console.log(JSON.stringify(args));
   }
--- test/passing-arguments.js
+++ test/passing-arguments.js
@@ -2,9 +2,31 @@ import { test } from 'tap'
 
 import { command } from '../command'
 
+/**
+ * The following assertions rely on the implementation of `../examples/echo:run`
+ * to print the `JSON.stringify`ed arguments to stdout.
+ *
+ * Passing any argument via shell means they are all received as strings,
+ * there is no conversion of arguments (other then type coercion done by JS runtime).
+ *
+ * @see examples/echo.js
+ */
+test('calls run method without any argument', async t => {
+  [
+    ['via shebang', `./index.js ./examples/echo`],
+    ['via node', `node ./index.js ./examples/echo`],
+    ['via npx', `npx . ./examples/echo`],
+  ].forEach(([msg, cmd]) => {
+    t.test(msg, async t => {
+      return command(cmd).then(it => {
+        t.equal(it.stdout.trim(), '[]')
+      })
+    })
+  })
+})
+
 test('calls run method and passes single argument', async t => {
-  const args = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const argsJoined = args.join(' ');
+  const argsJoined = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].join(' ');
   [
     ['via shebang', `./index.js ./examples/echo '${argsJoined}'`],
     ['via node', `node ./index.js ./examples/echo '${argsJoined}'`],
@@ -12,7 +34,7 @@ test('calls run method and passes single argument', async t => {
   ].forEach(([msg, cmd]) => {
     t.test(msg, async t => {
       return command(cmd).then(it => {
-        t.match(it.stdout.trim(), JSON.stringify([argsJoined]))
+        t.match(it.stdout.trim(), `["${argsJoined}"]`)
       })
     })
   })
@@ -21,7 +43,6 @@ test('calls run method and passes single argument', async t => {
 test('calls run method and passes arguments', async t => {
   const args = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   const argsJoined = args.join(' ');
-  const strings = args.map(it => it.toString());
   [
     ['via shebang', `./index.js ./examples/echo ${argsJoined}`],
     ['via node', `node ./index.js ./examples/echo ${argsJoined}`],
@@ -29,7 +50,7 @@ test('calls run method and passes arguments', async t => {
   ].forEach(([msg, cmd]) => {
     t.test(msg, async t => {
       return command(cmd).then(it => {
-        t.match(it.stdout.trim(), JSON.stringify(strings))
+        t.match(it.stdout.trim(), '["0","1","2","3","4","5","6","7","8","9"]')
       })
     })
   })
